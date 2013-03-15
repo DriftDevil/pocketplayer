1c9082c56f2c0e6a117e390dabeb5b600f354889
Kenny Huang <kthuang09@gmail.com>
2013-03-15 12:03:00 -0400

    Initial setup

---
+++ PocketPlayer/src/edu/psu/ktwok/pocketplayer/SplashScreen.java
@@ -0,0 +1,35 @@
+package edu.psu.ktwok.pocketplayer;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class SplashScreen extends Activity {
+	Intent intent;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash_screen, menu);
+		return true;
+	}
+	
+	@Override
+	public void onBackPressed() {
+		finish();
+	}
+
+	public void goToTitle(View v) {
+		intent = new Intent(this, TitleScreen.class);
+		startActivity(intent);
+	}
+}
