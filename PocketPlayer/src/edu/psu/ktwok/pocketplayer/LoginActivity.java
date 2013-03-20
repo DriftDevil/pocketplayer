043667be3a623e8d79b2aace7e26a8a7b39831af
Kenny Huang <kthuang09@gmail.com>
2013-03-20 13:06:27 -0400

    registration and login screen and linking to it

--- PocketPlayer/src/edu/psu/ktwok/pocketplayer/LoginActivity.java
+++ PocketPlayer/src/edu/psu/ktwok/pocketplayer/LoginActivity.java
@@ -2,14 +2,27 @@ package edu.psu.ktwok.pocketplayer;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.login);
+		TextView regScreen = (TextView) findViewById(R.id.link_to_register);
+		
+		regScreen.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+				startActivity(i);
+			}
+		});
 	}
 
 	@Override
