67201042a0db42cd7bd190eb174a3fd00a43a582
Kenny Huang <kthuang09@gmail.com>
2013-03-24 20:55:36 -0400

    Db Handler

--- PocketPlayer/src/edu/psu/ktwok/pocketplayer/DatabaseHandler.java
+++ PocketPlayer/src/edu/psu/ktwok/pocketplayer/DatabaseHandler.java
@@ -1,6 +1,11 @@
 package edu.psu.ktwok.pocketplayer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -30,22 +35,27 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
     	
-    	// TODO: set primary keys //
-    	
     	// Creating the Registration Table
     	String CREATE_REGISTRATION_TABLE = "CREATE TABLE " + TABLE_REGISTRATION + "("
-    			+ KEY_NAME + " TEXT,"
-    			+ KEY_EMAIL + " TEXT,"
+    			+ KEY_ID + " INTEGER PRIMARY KEY,"
+    			+ KEY_NAME + " TEXT NOT NULL,"
+    			+ KEY_EMAIL + " TEXT NOT NULL,"
     			+ KEY_PASSWORD + " TEXT" + ")";
     	db.execSQL(CREATE_REGISTRATION_TABLE);
     	
     	// Creating the Login Table
     	String CREATE_LOGIN_TABLE = "CREATE TABLE " + TABLE_LOGIN + "("
-    			+ KEY_EMAIL + " TEXT,"
-    			+ KEY_PASSWORD + " TEXT" + ")";
+    			+ KEY_ID + " INTEGER PRIMARY KEY,"
+    			+ KEY_EMAIL + " TEXT NOT NULL,"
+    			+ KEY_PASSWORD + " TEXT NOT NULL" + ")";
     	db.execSQL(CREATE_LOGIN_TABLE);
     	
-    	
+    	// Creating the Contacts Table
+    	String CREATE_CONTACTS_TABLE = "CREAT TABLE " + TABLE_CONTACTS + "("
+    			+ KEY_ID + " INTEGER PRIMARY KEY,"
+    			+ KEY_NAME + " TEXT NOT NULL,"
+    			+ KEY_EMAIL + " TEXT NOT NULL" + ")";
+    	db.execSQL(CREATE_CONTACTS_TABLE);    	
     }
     
     // Upgrading Database
@@ -57,5 +67,94 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     	// Create tables again
     	onCreate(db);
     }
+    
+    // Adding new contact
+    public void addContact(Users contact) {
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	
+    	ContentValues values = new ContentValues();
+    	values.put(KEY_NAME, contact.getName()); // Contact name
+    	values.put(KEY_EMAIL, contact.getEmail()); // Contact email
+    	
+    	// Inserting row
+    	db.insert(TABLE_CONTACTS, null, values);
+    	db.close(); // Closing database connection
+    }
+    
+    // Getting single contact
+    public Users getContact(int id) {
+    	SQLiteDatabase db = this.getReadableDatabase();
+    	
+    	Cursor cursor = db.query(TABLE_CONTACTS, new String[] {KEY_ID, KEY_NAME, KEY_EMAIL}, KEY_ID + "=?", 
+    											new String[] {String.valueOf(id)}, null, null, null, null);
+    	if (cursor != null) {
+    		cursor.moveToFirst();
+    	}
+    	
+    	Users contact = new Users(Integer.parseInt(cursor.getString(0)),
+    					cursor.getString(1), cursor.getString(2));
+    	
+    	// Return contact
+    	return contact;
+    }
+    
+    // Getting all contacts
+    public List<Users> getAllContacts() {
+    	List<Users> contactList = new ArrayList<Users>();
+    	// Select All query
+    	String selectQuery = "SELECT * FROM " + TABLE_CONTACTS;
+    	
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	Cursor cursor = db.rawQuery(selectQuery, null);
+    	
+    	// Looping through all rows and adding to list
+    	if (cursor.moveToFirst()) {
+    		do {
+    			Users contact = new Users();
+    			contact.setId(Integer.parseInt(cursor.getString(0)));
+    			contact.setName(cursor.getString(1));
+    			contact.setEmail(cursor.getString(2));
+    			// Adding contact to list
+    			contactList.add(contact);
+    		}
+    		while (cursor.moveToNext());
+    	}
+    	// Return contact list
+    	return contactList;
+    }
+    
+    // Updating single contact
+    public int updateContact(Users contact) {
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	
+    	ContentValues values = new ContentValues();
+    	values.put(KEY_NAME, contact.getName());
+    	values.put(KEY_EMAIL, contact.getEmail());
+    	
+    	// Updating row
+    	return db.update(TABLE_CONTACTS, values, KEY_ID + " = ?", 
+    			new String[] {String.valueOf(contact.getId())});
+    }    
+    
+    // Deleting single contact
+    public void deleteContact(Users contact) {
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	db.delete(TABLE_CONTACTS, KEY_ID + " = ?", 
+    			new String[] {String.valueOf(contact.getId())});
+    	db.close();
+    }
+    
+    // Getting contacts count
+    public int getContactsCount() {
+    	String countQuery = "SELECT * FROM " + TABLE_CONTACTS;
+    	SQLiteDatabase db = this.getReadableDatabase();
+    	Cursor cursor = db.rawQuery(countQuery, null);
+    	cursor.close();
+    	
+    	// Return count
+    	return cursor.getCount();
+    }
+
+
 	
 }
