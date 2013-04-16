1509fe9092fb2ce2f3d9aeee329cf7127a23ee5e
Kenny Huang <kthuang09@gmail.com>
2013-04-16 19:06:02 -0400

    char

--- PocketPlayer/src/edu/psu/ktwok/pocketplayer/CharSelect.java
+++ PocketPlayer/src/edu/psu/ktwok/pocketplayer/CharSelect.java
@@ -1,10 +1,14 @@
 package edu.psu.ktwok.pocketplayer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class CharSelect extends Activity{
+	Intent i;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,5 +20,10 @@ public class CharSelect extends Activity{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
+	}
+	
+	public void goToCharInfo(View v) {
+		i = new Intent(this, CharInfo.class);
+		startActivity(i);
 	}
 }
