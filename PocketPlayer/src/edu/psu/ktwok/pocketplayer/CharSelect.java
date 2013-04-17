b00881f447273673c6e25926db9c27b318ce0a3f
Kenny Huang <kthuang09@gmail.com>
2013-04-16 23:26:13 -0400

    charselect

--- PocketPlayer/src/edu/psu/ktwok/pocketplayer/CharSelect.java
+++ PocketPlayer/src/edu/psu/ktwok/pocketplayer/CharSelect.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class CharSelect extends Activity{
@@ -18,12 +19,25 @@ public class CharSelect extends Activity{
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
+		getMenuInflater().inflate(R.menu.activity_character_select, menu);	
 		return true;
 	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.char_info:
+			i = new Intent(this, ContactMail.class);
+			startActivity(i);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+		
+	}
+	
 	public void goToCharInfo(View v) {
-		i = new Intent(this, ContactMail.class);
+		i = new Intent(this, CharInfo.class);
 		startActivity(i);
 	}
 }
