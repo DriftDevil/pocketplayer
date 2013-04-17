b8bb3e5ef575bd0535a04d64bd5ae95d18745705
Kenny Huang <kthuang09@gmail.com>
2013-04-16 20:06:25 -0400

    skills

--- PocketPlayer/src/edu/psu/ktwok/pocketplayer/SkillFrag.java
+++ PocketPlayer/src/edu/psu/ktwok/pocketplayer/SkillFrag.java
@@ -1,17 +1,30 @@
 package edu.psu.ktwok.pocketplayer;
 
-import android.app.Fragment;
+import android.app.ListFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 
-public class SkillFrag extends Fragment{
+public class SkillFrag extends ListFragment{
 
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState){
 		return (LinearLayout) inflater.inflate(R.layout.skill_tab, container, false);
+	}
+	
+	private Context context;
+	
+	public void onActivityCreated(Bundle savedInstanceState){
+		super.onSaveInstanceState(savedInstanceState);
+		String [] skills = context.getResources().getStringArray(R.array.skills);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, skills);
+		setListAdapter(adapter);
 	}
 }
